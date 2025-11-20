36ab6c3de4bbc68480b3e73bba01eb0ae75dbde7
ChristianMarcellino <christianmarcellino14@gmail.com>
2025-11-20 18:42:33 +0700

    Integration

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, jsonify, request
+import pandas as pd
+import joblib
+
+app = Flask(__name__)
+
+loaded_model = joblib.load("models/diabetes_model.pkl")
+loaded_scaler = joblib.load("models/standard_scaler.pkl")
+columns = [
+    "Pregnancies",
+    "Glucose",
+    "BloodPressure",
+    "SkinThickness",
+    "Insulin",
+    "BMI",
+    "DiabetesPedigreeFunction",
+    "Age"
+]
+
+@app.route('/')
+def index():
+    return jsonify({
+        "meta" : {
+            "status" : "Success",
+            "message" : "Welcome to Diabetes API"
+        },
+        "data" : None
+    })
+
+@app.route('/api/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+
+    x_input = pd.DataFrame([data], columns=columns)
+
+    x_input_scaled = loaded_scaler.transform(x_input)
+
+    prediction = loaded_model.predict(x_input_scaled)
+
+    return jsonify({
+        "meta" : {
+            "status" : "Success",
+            "message" : "Predict diabetes using a trained model"
+        },
+        "data" : "Positive Diabetes" if prediction.tolist()[0] == 1 else "Negative Diabetes"
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ frontend/src/api/pages/Predict.jsx
@@ -0,0 +1,11 @@
+export default function Predict() {
+
+    const [form, setForm] = []
+    const handleFormChange(e){
+        
+    }
+
+  return (
+    <div>Predict</div>
+  )
+}
